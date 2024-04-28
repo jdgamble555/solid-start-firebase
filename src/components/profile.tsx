c34a3f0447273abdb4212a627e79ad27b55e432e
Jonathan Gamble <jdgamble555@gmail.com>
2024-04-28 11:06:31 -0500

    profile show

--- src/components/profile.tsx
+++ src/components/profile.tsx
@@ -7,23 +7,21 @@ export default function Profile() {
 
     const [user] = useUser();
 
-    if (!user.data) {
-        return;
-    }
-
-    const { displayName, photoURL, uid } = user.data;
-
     return (
-        <div class="flex flex-col gap-3 items-center">
-            <h3 class="font-bold">Hi {displayName}!</h3>
-            <Show when={photoURL}>
-                {(data) => (
-                    <img src={data()} width="100" height="100" alt="user avatar" />
-                )}
-            </Show>
-            <p>Your userID is {uid}</p>
-            <Logout />
-            <Todos />
-        </div>
+        <Show when={user.data}>
+            {(user) => (
+                <div class="flex flex-col gap-3 items-center">
+                    <h3 class="font-bold">Hi {user().displayName}!</h3>
+                    <Show when={user().photoURL}>
+                        {(data) => (
+                            <img src={data()} width="100" height="100" alt="user avatar" />
+                        )}
+                    </Show>
+                    <p>Your userID is {user().uid}</p>
+                    <Logout />
+                    <Todos />
+                </div>
+            )}
+        </Show>
     );
 }
